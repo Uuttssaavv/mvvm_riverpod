0f591ad5ded0659544bad5c8a813f3057d88c356
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-24 01:24:26 +0545

    created data class for user

---
+++ lib/models/user_model.dart
@@ -0,0 +1,47 @@
+class User {
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
+  final String token;
+
+  const User({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.token,
+  });
+  factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"],
+        username: json["username"],
+        email: json["email"],
+        firstName: json["firstName"],
+        lastName: json["lastName"],
+        gender: json["gender"],
+        image: json["image"],
+        token: json["token"],
+      );
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "username": username,
+        "email": email,
+        "firstName": firstName,
+        "lastName": lastName,
+        "gender": gender,
+        "image": image,
+        "token": token,
+      };
+  Map<String, dynamic> toDisplayMap() => {
+        "name": '$firstName $lastName',
+        "username": username,
+        "email": email,
+        "gender": gender,
+      };
+}
