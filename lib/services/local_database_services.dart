c9ca25109f6c16778c7b48a283e3b7e974002221
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-25 22:56:59 +0545

    added CRUD operations

--- lib/services/local_database_services.dart
+++ lib/services/local_database_services.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_project/models/db_product_db_model.dart';
+import 'package:flutter_project/models/product.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -12,7 +13,8 @@ class DatabaseService {
   Future<Database> get database async {
     if (_database != null) return _database!;
 
-    _database = await _initDB('notes.db');
+    _database = await _initDB('products.db');
+    print(_database?.isOpen);
     return _database!;
   }
 
@@ -35,6 +37,62 @@ CREATE TABLE $tableName (
   ${ProductDatabaseEntity.dateAdded} $textType
   )
 ''');
+  }
+
+  Future<Product> create(Product product) async {
+    final db = await instance.database;
+
+    final id = await db.insert(tableName, product.toMap());
+    return product.copyWith(id: id);
+  }
+
+  Future<Product> readNote(int id) async {
+    final db = await instance.database;
+
+    final maps = await db.query(
+      tableName,
+      columns: ProductDatabaseEntity.values,
+      where: '${ProductDatabaseEntity.id} = ?',
+      whereArgs: [id],
+    );
+
+    if (maps.isNotEmpty) {
+      return Product.fromMap(maps.first);
+    } else {
+      throw Exception('ID $id not found');
+    }
+  }
+
+  Future<List<Product>> readAllNotes() async {
+    final db = await instance.database;
+
+    const orderBy =
+        '${ProductDatabaseEntity.dateAdded} ASC'; //this will order products by date added in ascending order
+
+    final result = await db.query(tableName, orderBy: orderBy);
+
+    return result.map((json) => Product.fromMap(json)).toList();
+  }
+
+  Future<int> update(Product product) async {
+    final db = await instance.database;
+
+    return db.update(
+      tableName,
+      product.toMap(),
+      where: '${ProductDatabaseEntity.id} = ?',
+      whereArgs: [product.id],
+    );
+  }
+
+  Future<int> delete(int id) async {
+    final db = await instance.database;
+
+    return await db.delete(
+      tableName,
+      where: '${ProductDatabaseEntity.id} = ?',
+      whereArgs: [id],
+    );
   }
 
   Future close() async {
